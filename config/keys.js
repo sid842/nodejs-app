9f244543ca6995c495d29df2cd9d24a52ded8a6c
sid <sidbhasin82@gmail.com>
2020-01-17 20:23:33 +0530

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://sid:sid@cluster0-shard-00-00-47zsj.mongodb.net:27017,cluster0-shard-00-01-47zsj.mongodb.net:27017,cluster0-shard-00-02-47zsj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+};
